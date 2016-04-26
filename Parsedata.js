c64366ae01b6cebba2cfe725ddf2d1a5de68a99b
Amita Singh Chauhan <singhchauhan.amita@gmail.com>
2016-04-26 13:41:08 +0200

    Add acknowledgemnts.

--- Parsedata.js
+++ Parsedata.js
@@ -1,14 +1,14 @@
 var results = null;
 var template = '' +
-    '<div class="row spacing">' +
-	 '<div class="col-md-8 col-xs-12 quote" id="quote-comp">$quote$</div>' +
-	 '<div class="col-md-1 col-xs-1 tilde">~</div>' +
-	 '<div class="col-md-3 col-xs-11 quote" id="contributor-name">$contributor$</div>' +
-    '</div>';
+'<div class="row spacing">' +
+'<div class="col-md-8 col-xs-12 quote" id="quote-comp">$quote$</div>' +
+'<div class="col-md-1 col-xs-1 tilde">~</div>' +
+'<div class="col-md-3 col-xs-11 quote" id="contributor-name">$contributor$</div>' +
+'</div>';
 
 function getQuoteHTML(quote) {
 	return template.replace('$quote$', quote[0])
-	               .replace('$contributor$', quote[1]);
+	.replace('$contributor$', quote[1]);
 }
 
 var config = {
@@ -19,11 +19,11 @@ var config = {
 		var arrayLength= results.data.length;
 		var html = '';
 		for (var i=0; i < arrayLength; i++) {
-		    html += getQuoteHTML(results.data[i]);
+			html += getQuoteHTML(results.data[i]);
 		}
 
 		var quoteContainer = document.getElementById('foo-bar');
-    	quoteContainer.innerHTML = html;
+		quoteContainer.innerHTML = html;
 
 	},
 	error: function (err) {
