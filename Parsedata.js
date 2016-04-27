a71f931d15c7ede97e13e10344eede5ef5afd9ab
Amita Singh Chauhan <singhchauhan.amita@gmail.com>
2016-04-27 12:44:20 +0200

    Use random verbs.

--- Parsedata.js
+++ Parsedata.js
@@ -1,14 +1,24 @@
-var results = null;
+'use strict';
+
+
+function getWord() {
+	var words = ['said to','yelled at', 'whispered to',
+	 			'addressed to', 'shared with', 'taught to',
+				'preached to', 'shot at', 'gibed at'];
+	var n = Math.floor(Math.random() * words.length);
+	return words[n];
+}
 
 
 var template = ''+
 '<blockquote>' +
 '<p class="quote" id="quote-comp">$quote$</p>' +
-'<footer class="quote" id="contributor-name">said to $contributor$</footer>' +
+'<footer class="quote" id="contributor-name"> <span class="verb" id="verb-font"> $verb$ </span> $contributor$</footer>' +
 '</blockquote>';
 
 function getQuoteHTML(quote) {
 	return template.replace('$quote$', quote[0])
+	.replace('$verb$', getWord())
 	.replace('$contributor$', quote[1]);
 }
 
